146bb64a106056c45c6bcf810ecd51b9958ae19e
jrg05 <jrg.gmnz@gmail.com>
2026-05-16 14:16:04 -0300

    Delete ios.conf.js

--- ios.conf.js
+++
@@ -1,9 +0,0 @@
-const { config } = require('../wdio.conf');
-
-config.capabilities = [{
-  platformName: 'iOS',
-  'appium:deviceName': 'iPhone 15',
-  'appium:automationName': 'XCUITest'
-}];
-
-exports.config = config;
