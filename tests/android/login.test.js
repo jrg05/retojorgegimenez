8c2d92ddcd04ff8594476631efc3a081c93734d8
jrg05 <jrg.gmnz@gmail.com>
2026-05-15 21:46:46 -0300

    Feat: Add Android login test

---
+++ tests/android/login.test.js
@@ -0,0 +1,7 @@
+const LoginPage = require("../../pages/LoginPage");
+
+describe("Android Login Flow", () => {
+  it("should login successfully on Android", async () => {
+    await LoginPage.login("standard_user", "secret_sauce");
+  });
+});
