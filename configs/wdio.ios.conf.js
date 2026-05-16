3459f73c5cd50b64ff2db142a7b50e8d98e2dca5
jrg05 <jrg.gmnz@gmail.com>
2026-05-16 14:27:43 -0300

    Refactor: Ajuste por archivo capabilities.js

--- configs/wdio.ios.conf.js
+++ configs/wdio.ios.conf.js
@@ -1,21 +1,14 @@
+const baseConfig = require("./wdio.conf");
+const capabilities = require("./capabilities");
+
 exports.config = {
-  runner: "local",
+  ...baseConfig.config,
 
-  port: 4723,
+  specs: [
+    "../tests/ios/**/*.test.js"
+  ],
 
-  specs: ["../tests/ios/**/*.js"],
-
-  capabilities: [{
-    platformName: "iOS",
-    "appium:deviceName": "iPhone Simulator",
-    "appium:automationName": "XCUITest",
-    "appium:bundleId": "com.saucelabs.mydemoapp.ios",
-    "appium:noReset": true
-  }],
-
-  framework: "mocha",
-  services: ["appium"],
-  mochaOpts: {
-    timeout: 60000
-  }
+  capabilities: [
+    capabilities.ios
+  ]
 };
