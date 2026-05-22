141239d8607e4cedbb713dd3762d6ebf779ef9ca
jrg05 <jrg.gmnz@gmail.com>
2026-05-22 20:56:41 -0300

    Refactor login test

--- tests/android/login.test.js
+++ tests/android/login.test.js
@@ -1,7 +1,14 @@
 const LoginPage = require("../../pages/LoginPage");
 
 describe("Android Login Flow", () => {
+
   it("should login successfully on Android", async () => {
-    await LoginPage.login("standard_user", "secret_sauce");
+
+    await LoginPage.login(
+      "standard_user",
+      "secret_sauce"
+    );
+
   });
+
 });
