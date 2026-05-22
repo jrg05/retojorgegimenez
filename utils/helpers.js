bfb548aa6b7e335aa6d4f9abd72c0b2fd175cbf2
jrg05 <jrg.gmnz@gmail.com>
2026-05-22 20:54:51 -0300

    Refactor screenshot function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,16 @@
 const fs = require("fs");
 
-async function screenshot(driver, name) {
-  const img = await driver.takeScreenshot();
-  fs.writeFileSync(`evidence/${name}.png`, img, "base64");
+async function takeScreenshot(name) {
+
+  const image = await browser.takeScreenshot();
+
+  fs.writeFileSync(
+    `evidence/screenshots/${name}.png`,
+    image,
+    "base64"
+  );
 }
 
-module.exports = { screenshot };
+module.exports = {
+  takeScreenshot
+};
