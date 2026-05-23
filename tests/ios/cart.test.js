57d9b137756d1de89217e731dca8fe5cb19b6f95
jrg05 <jrg.gmnz@gmail.com>
2026-05-22 21:01:19 -0300

    Refactor iOS checkout test

--- tests/ios/cart.test.js
+++ tests/ios/cart.test.js
@@ -3,12 +3,20 @@ const ProductsPage = require("../../pages/ProductsPage");
 const CartPage = require("../../pages/CartPage");
 
 describe("iOS Checkout Flow", () => {
-  it("should complete purchase on iOS", async () => {
-    await LoginPage.login("standard_user", "secret_sauce");
 
-    await ProductsPage.selectProduct();
-    await ProductsPage.addToCartItem();
+  it("should complete checkout flow", async () => {
 
-    await CartPage.checkout();
+    await LoginPage.login(
+      "standard_user",
+      "secret_sauce"
+    );
+
+    await ProductsPage.selectFirstProduct();
+
+    await ProductsPage.addProductToCart();
+
+    await CartPage.proceedToCheckout();
+
   });
+
 });
